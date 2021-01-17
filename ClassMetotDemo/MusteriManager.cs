ae60cb39aeee02c50f9cdad64d6a3152e2ec167d
tolga hastaoğlu <tolga_hastaoglu@hotmail.com>
2021-01-17 17:52:50 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle()
+        {
+            Musteri ekle = new Musteri();
+            ekle.Id = 5;
+            Console.Write("Yeni Müşteri Adı Giriniz: ");
+            ekle.Adi = Console.ReadLine();
+            Console.Write("Yeni Müşteri Soyadı Giriniz: ");
+            ekle.Soyadii = Console.ReadLine();
+            Console.WriteLine("Yeni Müşteri Eklenmiştir");
+        }
+        public void Sil()
+        {
+            Console.Write("Silinecek Müşteri ID giriniz: ");
+            
+           
+        }
+
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Menü");
+            Console.WriteLine("1 Müşteri ekleme");
+            Console.WriteLine("2 Müşteri Silme");
+            int secim = Convert.ToInt32(Console.ReadLine());
+            MusteriManager musterimanager = new MusteriManager();
+            if (secim==1)
+            {
+                musterimanager.Ekle();
+            }
+            else if (secim == 2)
+            {
+                musterimanager.Sil();
+            }
+            else
+            {
+                Console.WriteLine("Doğru seçim yapınız");
+            }
+        }
+    }
+}
